e8df9ce7f66c69c6fb3e2e5f724e737632a78e79
Ricky Romero <ricky.romero@gmail.com>
2020-05-29 17:56:20 -0700

    Add welcome image. Fix #1

--- Shut Up/Welcome/WelcomeViewController.swift
+++ Shut Up/Welcome/WelcomeViewController.swift
@@ -8,4 +8,39 @@
 
 import Cocoa
 
-class WelcomeViewController: NSViewController {}
+class WelcomeViewController: NSViewController {
+    @IBOutlet var babyBrowser: NSImageView!
+    @IBOutlet var babyBrowserAtBottom: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        NotificationCenter.default.addObserver(
+            forName: NSColor.systemColorsDidChangeNotification,
+            object: nil,
+            queue: .main,
+            using: updateBabyBrowserTintColor(_:)
+        )
+
+        updateBabyBrowserTintColor(nil)
+
+        babyBrowserAtBottom.isActive = false
+        babyBrowser.alphaValue = 0.0
+
+        Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) { _ in
+            NSAnimationContext.runAnimationGroup({ context in
+                context.duration = 1.333
+                context.allowsImplicitAnimation = true
+
+                self.babyBrowserAtBottom.isActive = true
+                self.babyBrowser.alphaValue = 1.0
+                self.view.window?.layoutIfNeeded()
+            })
+        }
+    }
+
+    func updateBabyBrowserTintColor(_: Notification?) {
+        let isAqua = NSColor.currentControlTint != NSControlTint.graphiteControlTint
+        let appearance = isAqua ? "Aqua" : "Graphite"
+
+        babyBrowser.image = NSImage(named: "Baby Browser \(appearance)")
+    }
+}
