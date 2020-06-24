9fd956bf5207dba8f1e22348773e8ac786b370a8
Ricky Romero <ricky.romero@gmail.com>
2020-06-23 23:06:01 -0700

    Hook up context menu. Fix #12

--- Shut Up Helper/SafariExtensionHandler.swift
+++ Shut Up Helper/SafariExtensionHandler.swift
@@ -21,45 +21,21 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
         Setup.main.bootstrap {}
     }
 
+    override func contextMenuItemSelected(withCommand command: String, in page: SFSafariPage, userInfo: [String : Any]? = nil) {
+        page.getContainingTab { tab in
+            tab.getContainingWindow { window in
+                page.getPropertiesWithCompletionHandler { props in
+                    self.toggleComments(for: window, with: page, having: props)
+                }
+            }
+        }
+    }
+
     override func toolbarItemClicked(in window: SFSafariWindow) {
         window.getActiveTab { tab in
             tab?.getActivePage { page in
                 page?.getPropertiesWithCompletionHandler { props in
-                    guard let domain = self.getDomain(from: props?.url) else { return }
-                    _ = Whitelist.main.toggle(domain: domain)
-
-                    window.getToolbarItem { button in
-                        self.updateIcon(in: button, for: props?.url)
-                    }
-
-                    SFContentBlockerManager.reloadContentBlocker(withIdentifier: Info.blockerBundleId) { error in
-                        guard error == nil else {
-                            NSLog("com.rickyromero.shutup.blocker helper error: \(error!.localizedDescription)")
-                            return
-                        }
-
-                        // This event lies, dispatching the completion handler before it's ready.
-                        // It's pretty consistently ready at about 10 milliseconds of delay on my system.
-                        // Hopefully 50 milliseconds is long enough of a pause to sort it out on every system.
-                        DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(50)) {
-                            page?.reload()
-                        }
-
-                        let shouldRemove = (
-                            !Preferences.main.automaticWhitelisting ||
-                            (props?.usesPrivateBrowsing ?? false)
-                        )
-                        if shouldRemove {
-                            // Unfortunately this also requires an ugly delay...
-                            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
-                                _ = Whitelist.main.remove(domains: [domain])
-                                SFContentBlockerManager.reloadContentBlocker(
-                                    withIdentifier: Info.blockerBundleId,
-                                    completionHandler: nil
-                                )
-                            }
-                        }
-                    }
+                    self.toggleComments(for: window, with: page, having: props)
                 }
             }
         }
@@ -79,6 +55,44 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
         }
     }
 
+    func toggleComments(for window: SFSafariWindow?, with page: SFSafariPage?, having props: SFSafariPageProperties?) {
+        guard let domain = self.getDomain(from: props?.url) else { return }
+        _ = Whitelist.main.toggle(domain: domain)
+
+        window?.getToolbarItem { button in
+            self.updateIcon(in: button, for: props?.url)
+        }
+
+        SFContentBlockerManager.reloadContentBlocker(withIdentifier: Info.blockerBundleId) { error in
+            guard error == nil else {
+                NSLog("com.rickyromero.shutup.blocker helper error: \(error!.localizedDescription)")
+                return
+            }
+
+            // This event lies, dispatching the completion handler before it's ready.
+            // It's pretty consistently ready at about 10 milliseconds of delay on my system.
+            // Hopefully 50 milliseconds is long enough of a pause to sort it out on every system.
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(50)) {
+                page?.reload()
+            }
+
+            let shouldRemove = (
+                !Preferences.main.automaticWhitelisting ||
+                (props?.usesPrivateBrowsing ?? false)
+            )
+            if shouldRemove {
+                // Unfortunately this also requires an ugly delay...
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+                    _ = Whitelist.main.remove(domains: [domain])
+                    SFContentBlockerManager.reloadContentBlocker(
+                        withIdentifier: Info.blockerBundleId,
+                        completionHandler: nil
+                    )
+                }
+            }
+        }
+    }
+
     func getDomain(from url: URL?) -> String? {
         guard let fullHost = url?.host else { return nil }
         return Whitelist.stripWww(from: fullHost)
@@ -90,10 +104,11 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
         let matched = Whitelist.main.matches(domain: domain)
         let icon = matched ? toolbarImages.disabled : toolbarImages.enabled
         button?.setImage(icon)
+        button?.setLabel(matched ? "Hide Comments" : "Show Comments")
     }
 
     override func validateContextMenuItem(withCommand command: String, in page: SFSafariPage, userInfo: [String : Any]? = nil, validationHandler: @escaping (Bool, String?) -> Void) {
-        validationHandler(false, nil)
+        validationHandler(!Preferences.main.showInMenu, nil)
     }
 }
 
