8d940c981b34e5f19ffe1f16fc007fdaea30dac4
Toni Förster <toni.foerster@icloud.com>
2025-02-18 12:33:39 +0100

    remove Catalina related warning message
    Signed-off-by: Toni Förster <toni.foerster@icloud.com>

--- Shared/MessagingError.swift
+++ Shared/MessagingError.swift
@@ -22,7 +22,6 @@ enum CryptoError: Error {
     case generatingKeys
     case fetchingKeys
     case transformingData
-    case migratingPreCatalinaKeys
 }
 
 enum FileError: Error {
@@ -120,10 +119,6 @@ class MessagingError: NSError, @unchecked Sendable {
                 case .transformingData:
                     title = "Stylesheet or allowlist damaged"
                     info = "Shut Up failed to decrypt some required data. You can fix this by resetting Shut Up, but your allowlist may be lost."
-                    options = [.quit, .reset]
-                case .migratingPreCatalinaKeys:
-                    title = "Key migration failed"
-                    info = "Shut Up tried to migrate encryption keys from an older version of macOS, but it failed. You can fix this by resetting Shut Up, but your allowlist may be lost."
                     options = [.quit, .reset]
             }
         } else if cause is LockError {
